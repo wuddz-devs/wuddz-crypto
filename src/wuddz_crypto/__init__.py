5324560b94ff9f45417ecfd71027a140a73e98b1
wuddz-devs <wuddz_devs@protonmail.com>
2024-01-22 14:53:01 -0400

    deleted

--- src/wuddz_crypto/__init__.py
+++
@@ -1,4 +0,0 @@
-import sys
-from pathlib import Path
-__all__ = [x.stem for x in Path(Path(__file__).absolute().parent).glob('*.py') if not str(x).startswith('__init')]
-if str(Path(__file__).absolute().parent) not in sys.path:sys.path.insert(1, str(Path(__file__).absolute().parent))
